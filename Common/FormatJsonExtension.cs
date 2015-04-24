61b1954d891e865da11be3f5d29cb51141725352
islandof <islandof@vip.qq.com>
2015-04-24 13:42:14 +0800

    BlossomWeb

---
+++ Common/FormatJsonExtension.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace BlossomWeb.Common
+{
+    public static class FormatJsonExtension
+    {
+        public static FormatJsonResult JsonFormat(this Controller c, object data)
+        {
+            var result = new FormatJsonResult();
+            result.NotLigerUIFriendlySerialize = true;
+            result.Data = data;
+            return result;
+        }
+
+    }
+
+    /// <summary>
+    ///     JsonResult格式化扩展
+    /// </summary>
+    public class FormatJsonResult : ActionResult
+    {
+        /// <summary>
+        ///     是否产生错误
+        /// </summary>
+        public bool IsError { get; set; }
+
+        /// <summary>
+        ///     错误信息，或者成功信息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        ///     成功可能时返回的数据
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        ///     正常序列化方式(为True则不进行UI友好的序列化)
+        /// </summary>
+        public bool NotLigerUIFriendlySerialize { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+
+            var sw = new StringWriter();
+            JsonSerializer serializer =
+                JsonSerializer.Create(
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        NullValueHandling = NullValueHandling.Ignore
+                    });
+
+            using (JsonWriter jsonWriter = new JsonTextWriter(sw))
+            {
+                jsonWriter.Formatting = Formatting.Indented;
+                serializer.Serialize(jsonWriter, !NotLigerUIFriendlySerialize ? this : Data);
+            }
+
+            response.Write(sw.ToString());
+        }
+    }
+
+    public class EasyUIGrid
+    {
+        /// <summary>
+        ///     返回的记录
+        /// </summary>
+        public object rows { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the data.
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the jobnums.
+        /// </summary>
+        public object jobnums { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the wattypes.
+        /// </summary>
+        public object wattypes { get; set; }
+
+        /// <summary>
+        ///     总个数
+        /// </summary>
+        public int total { get; set; }
+
+        /// <summary>
+        ///     The to string.
+        /// </summary>
+        /// <returns>
+        ///     The <see cref="string" />.
+        /// </returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(
+                this, Formatting.Indented, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, });
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Web.Mvc;
+using BlossomWeb.Common;
+using Microsoft.Ajax.Utilities;
+
+namespace BlossomWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static readonly string ConnStr1 = ConfigurationManager.AppSettings["AccessConnString"] +
+                                                 System.Web.HttpContext.Current.Server.MapPath(
+                                                     ConfigurationManager.AppSettings["AccessDbPath"]) + ";";
+
+        //public static readonly string connStr1 = "Provider = Microsoft.ACE.OLEDB.12.0 ;Data Source=/App_Data/Blossom.accdb";
+
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult GetProducts()
+        {
+            var conn = new OleDbConnection(ConnStr1);
+
+            conn.Open();
+            var sql = "select * from Products";
+            var myadapter = new OleDbDataAdapter(sql, conn);
+            var dt = new DataTable();
+            myadapter.Fill(dt);
+
+            return this.JsonFormat(new EasyUIGrid
+            {
+                rows = dt,
+                total = 0
+            });
+        }
+
+        public ActionResult AddProduct()
+        {
+            ViewBag.CurrentID = Request["ID"];
+
+            return View();
+        }
+
+        public ActionResult SaveProduct()
+        {
+            var sql = "";
+            if (!Request.Form["Cnname"].IsNullOrWhiteSpace())
+            {
+                sql = Request.Form["Cnname"];
+            }
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file.ContentLength == 0)
+                {
+                    //文件大小大（以字节为单位）为0时，做一些操作
+                }
+                else
+                {
+                    //文件大小不为0
+                    //保存成自己的文件全路径,newfile就是你上传后保存的文件,
+                    //服务器上的UpLoadFile文件夹必须有读写权限　　　　　　
+                    file.SaveAs(Server.MapPath(@"\UploadFile\\newfile"));
+                }
+            }
+            return this.JsonFormat("");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
